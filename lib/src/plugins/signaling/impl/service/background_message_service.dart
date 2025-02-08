797a6394a75accb111dc0a88e315509a8c6325c3
yuyanjun <yuyanjun@zego.im>
2025-02-08 10:57:54 +0800

    Support for additional listening offline message handler

--- lib/src/plugins/signaling/impl/service/background_message_service.dart
+++ lib/src/plugins/signaling/impl/service/background_message_service.dart
@@ -3,12 +3,36 @@ import 'package:zego_plugin_adapter/zego_plugin_adapter.dart';
 
 /// @nodoc
 mixin ZegoPluginBackgroundMessageService {
+  /// register background message handler
+  /// only for Android
+  ///
+  ///
+  /// ```dart
+  /// @pragma('vm:entry-point')
+  /// Future<void> handler1(ZPNsMessage message) async {
+  ///     String title,
+  ///     String content,
+  ///     Map<String, Object?> extras,
+  ///     ZegoUIKitCallPushSourceType pushSourceType,
+  ///     ) {
+  ///   debugPrint(
+  ///       'handler 1 recv, title:$title, content:$content, extras:$extras, push type :$pushSourceType');
+  /// }
+  ///
+  /// await ZegoUIKit().getSignalingPlugin().setBackgroundMessageHandler(
+  /// handler1,
+  /// key: 'handler1',
+  /// );
+  /// ```
   Future<ZegoSignalingPluginSetMessageHandlerResult>
       setBackgroundMessageHandler(
-          ZegoSignalingPluginZPNsBackgroundMessageHandler handler) async {
-    return ZegoPluginAdapter()
-        .signalingPlugin!
-        .setBackgroundMessageHandler(handler);
+    ZegoSignalingPluginZPNsBackgroundMessageHandler handler, {
+    String key = 'default',
+  }) async {
+    return ZegoPluginAdapter().signalingPlugin!.setBackgroundMessageHandler(
+          handler,
+          key: key,
+        );
   }
 
   Future<ZegoSignalingPluginSetMessageHandlerResult> setThroughMessageHandler(
