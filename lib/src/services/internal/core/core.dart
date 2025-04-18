261fba1773c927afdc496afc2758f56aa7df1186
yuyanjun <yuyanjun@zego.im>
2025-04-18 15:53:33 +0800

    update zpns

--- lib/src/services/internal/core/core.dart
+++ lib/src/services/internal/core/core.dart
@@ -69,7 +69,7 @@ class ZegoUIKitCore
       final mobileInfo = 'platform:${Platform.operatingSystem}, '
           'version:${Platform.operatingSystemVersion}';
 
-      const zegoUIKitVersion = 'zego_uikit: 2.28.11; ';
+      const zegoUIKitVersion = 'zego_uikit: 2.28.12; ';
       version ??=
           '${zegoUIKitVersion}zego_express:$expressVersion,mobile:$mobileInfo';
     }
