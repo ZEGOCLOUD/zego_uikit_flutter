4288ab5840819dce528d963154b7d8c4dcc0e124
salahu01 <cvsalahu12@gmail.com>
2025-06-04 17:40:47 +0530

    refactor logger initialization to simplify log level configuration

--- lib/src/services/logger_service.dart
+++ lib/src/services/logger_service.dart
@@ -15,12 +15,7 @@ mixin ZegoLoggerService {
 
     try {
       await FlutterLogs.initLogs(
-              logLevelsEnabled: [
-                LogLevel.INFO,
-                LogLevel.WARNING,
-                LogLevel.ERROR,
-                LogLevel.SEVERE
-              ],
+              logLevelsEnabled: [LogLevel.INFO, LogLevel.WARNING, LogLevel.ERROR, LogLevel.SEVERE],
               timeStampFormat: TimeStampFormat.TIME_FORMAT_24_FULL,
               directoryStructure: DirectoryStructure.SINGLE_FILE_FOR_DAY,
               logTypesEnabled: ['device', 'network', 'errors'],
